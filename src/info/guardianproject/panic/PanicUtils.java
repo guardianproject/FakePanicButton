666c58a81e48ae2de47823ebf8d7ebe4795a9df6
Hans-Christoph Steiner <hans@eds.org>
2015-04-21 20:07:37 -0400

    handle null Calling Activity for Activities that are started by LAUNCHER
    If an Activity is started by the LAUNCHER IntentFilter or by startActivity,
    then the getCallingActivity() will be null.

--- src/info/guardianproject/panic/PanicUtils.java
+++ src/info/guardianproject/panic/PanicUtils.java
@@ -2,6 +2,7 @@
 package info.guardianproject.panic;
 
 import android.app.Activity;
+import android.content.ComponentName;
 import android.text.TextUtils;
 import android.util.Log;
 
@@ -9,7 +10,10 @@ public class PanicUtils {
 
     static String getCallingPackageName(Activity activity) {
         // getCallingPackage() was unstable until android-18, use this
-        String packageName = activity.getCallingActivity().getPackageName();
+        ComponentName componentName = activity.getCallingActivity();
+        if (componentName == null)
+            return null;
+        String packageName = componentName.getPackageName();
         if (TextUtils.isEmpty(packageName)) {
             packageName = activity.getIntent().getPackage();
         }
